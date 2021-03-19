50c692322d95e6e35eb1a0452931318efcb92601
Constantkavaya <kavayaconstant@gmail.comgit config --listgit config --global user.email kavayaconstant@gmail.com>
2021-03-19 20:19:58 +0300

    first commit

---
+++ lexicalscope.js
@@ -0,0 +1,12 @@
+function name() {
+    
+var a="shadya"
+var b="pamera"
+function identity(){
+   var  c= a + " " + "and" + b  + " "+ "are friends"
+    console.log(c);
+
+}
+identity();
+}
+name()
